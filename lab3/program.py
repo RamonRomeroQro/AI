0655f43ae901c0bc43d6777faf43b739b7d29c52
RamonRomeroTec <ramonromeroqro@protonmail.com>
2019-02-28 21:09:02 -0600

    finally

--- lab3/program.py
+++ lab3/program.py
@@ -1,21 +1,60 @@
+'''
+    Copyright 2019 © Ramón Romero @ramonromeroqro
+    Intelligent Systems, ITESM.
+    A01700318 for ITESM
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''
+
+
 from collections import deque
+import copy
 
 
 class GraphSearchSpace():
-    def __init__(self):
-        self.nodes = set()
-     
+    def __init__(self, m_h,):
+        self.nodes = {}
+        self.heuristic = {}
+        self.max_height = m_h
 
-        #n1_to_n2=Edge(node1, node2, o_stack, dest_stack)
-        #n2_to_n1=Edge(node1, node2, dest_stack, o_stack)
-        # self.edges.append(n1_to_n2)
-        # self.edges.append(n2_to_n1)
+    def generate_heuristic(self, goal_structure):
+        for k, v in self.nodes.items():
+            m1f = copy.deepcopy(v.structure)
+            m2f = copy.deepcopy(goal_structure)
+            m1f = self.filling(m1f)
+            m2f = self.filling(m2f)
+            #print('->>', v.structure,m1f )
+
+            self.heuristic[k] = self.evaluate(m1f, m2f)
+
+    def evaluate(self, m1, m2):
+        s = 0
+        for i in range(len(m1)):
+            for j in range(self.max_height):
+                if m1[i][j] == m2[i][j] and m1[i][j] != "*":
+                    s = s+1
+        return s
+
+    def filling(self, mat):
+        for s in mat:
+            if len(s) < self.max_height:
+                for i in range(0, abs(len(s)-self.max_height)):
+                    s.append('*')
+        return mat
 
     def __str__(self):
-        # s=""
-        # for i in self.nodes:
-        #     s=s+i.__str__()+"\n"
-        # return s
+
         s = ""
         for i in self.nodes:
             s = s+i.__str__()+"\n"
@@ -23,138 +62,285 @@ class GraphSearchSpace():
         return s
 
 
-class Edge():
-
-    def __init__(self, node2, o_stack, dest_stack):
-        self.destination = node2
-        self.origin_stack = o_stack
-        self.destination_stack = dest_stack
-        # Example: moving a container from stack 2 to stack 4 takes 0.5 + abs(2 - 4) + 0.5 = 3 minutes.
-        self.cost = 1+abs(o_stack-dest_stack)
-
-
-    def __str__(self):
-       return (f'--({self.cost}, {self.origin_stack}, {self.destination_stack})--> ')+str(self.destination)
-
-    def __eq__(self, other):
-        if isinstance(other, Edge):
-            return ((self.destination == other.destination) and (self.origin_stack == other.origin_stack) and (self.destination_stack == other.destination_stack) and (self.cost == other.cost))
-        else:
-            return False
-
-    def __ne__(self, other):
-        return (not self.__eq__(other))
-
-    def __hash__(self):
-        return hash((self.cost, self.destination_stack, self.origin_stack, self.destination))
-
-
 class Node:
 
-    def add_conection(self, n2, o_stack, dest_stack):
-        c = Edge(n2, o_stack, dest_stack)
-        self.conections.add(c)
-
     def __str__(self):
-        return self.structure.__str__()
+        return str(self.id)
 
-    def __eq__(self, other):
-        if isinstance(other, Node):
-            return ((self.structure == other.structure))
-        else:
-            return False
-
-    def __ne__(self, other):
-        return (not self.__eq__(other))
-
-    def __hash__(self):
-        return hash(tuple(map(tuple, self.structure)))
-
-    def __init__(self, structure, h):
+    def __init__(self, structure):
         self.num_stacks = len(structure)
-        self.max_height = h
         self.structure = structure
-        self.conections = set()
+        self.conections = {}
+        self.id = tuple(map(tuple, self.structure))
 
-    def generate_paths(self, gsp):
-        # print('genfor',self.structure)
-        #for i in range(0, self.num_stacks):
-        #   for j in range(0, self.num_stacks):
-        #        self.transition_model(i, j, gsp)
-
-        q=deque([i for i in range(0,self.num_stacks)])
-        for i in range(0,self.num_stacks):
-            e=q.popleft()
-            q.append(e)
-            for j in range(1+i,len(q)):
-        #         #print(i,j)
-                self.transition_model(i,j,gsp)
-                self.transition_model(j,i,gsp)
-
-    def transition_model(self, origin_stack, destination_stack, gsp):
-        if len(self.structure[destination_stack])+1 <= self.max_height and (len(self.structure[origin_stack])-1) >= 0:
+    def generate_node(self, origin_stack, destination_stack, gsp):
+        if len(self.structure[destination_stack])+1 <= gsp.max_height and (len(self.structure[origin_stack])-1) >= 0:
             new_structure = list(map(list, self.structure))
             e = new_structure[origin_stack].pop()
             new_structure[destination_stack].append(e)
-            new_node = Node(new_structure, self.max_height)
+            new_node = Node(new_structure)
+            return new_node
 
-            gsp.nodes.add(self)
+        else:
+            return None
 
-            evaluation_set=set()
-            evaluation_set.add(new_node)
-            evaluation_set=evaluation_set.intersection(gsp.nodes)
-            if len(evaluation_set)==0:
-                gsp.nodes.add(new_node)
-                # conect to new
-                #print(self.structure,'\t->\t', new_node.structure)
-                #gsp.add_conection(self, new_node, origin_stack, destination_stack)
-                self.add_conection(new_node, origin_stack, destination_stack)
-                new_node.generate_paths(gsp)
-            else:
-                for i in evaluation_set:
-                    self.add_conection(i, origin_stack, destination_stack)
+    def generate_paths(self, gsp):
+        execution_queue = deque([])
+        gsp.nodes[self.id] = self
+        execution_queue.append(self)
+        while(len(execution_queue)):
+            ex_node = execution_queue.popleft()
+            for i in range(0, ex_node.num_stacks):
+                for j in range(0, ex_node.num_stacks):
+                    if i != j:
+                        new_node = ex_node.generate_node(i, j, gsp)
+                        cost = 1+abs(i-j)
+                        if (new_node != None):
+                            if new_node.id not in gsp.nodes:
+                                gsp.nodes[new_node.id] = new_node
+                                ex_node.conections[(
+                                    i, j, cost)] = gsp.nodes[new_node.id]
+                                execution_queue.append(new_node)
+                            else:
+                                existent_node = gsp.nodes[new_node.id]
+                                ex_node.conections[(
+                                    i, j, cost)] = existent_node
+
+
+def ucs(start_node, goal_node):
+    explored = {}
+    pq = PriorityQueue()
+    # nodo, currentsume, path, heuristica
+    pq.add((start_node, 0, [], 0))
+    while(True):
+        # print(str(pq))
+        if pq.is_empty() == True:
+            return "No solution found"
+        nodo, latest_cost, path = pq.pop()
+        #print("...", path)
+        if nodo.id == goal_node.id:
+            explored[nodo.id] = nodo
+            # print(cost, nodo)
+            # print(path)
+            # print(latest_cost)
+            return str(latest_cost)+"\n"+"; ".join([str(x) for x in (path)])
+        # print("expanded ___!", nodo, cost)
+        for t, n in nodo.conections.items():  # expansion
+            conex = n
+            cost_n = t[2]
+            trans = (t[0], t[1])
+            n_path = list(path)
+            # print(n_path)
+            n_path.append(trans)
+            if conex.id not in explored:  # or conex not frontier:
+                # print(conex)
+                pq.add((conex, cost_n+latest_cost, n_path, 0))
+
+
+def a_star(start_node, goal_node, gsp):
+    explored = {}
+    pq = PriorityQueue()
+    # nodo, currentsume, path, heuristica
+    pq.add((start_node, 0, [], gsp.heuristic[start_node.id]))
+    while(True):
+        # print(str(pq))
+        if pq.is_empty() == True:
+            return "No solution found"
+        nodo, latest_cost, path = pq.pop()
+        #print("...", path)
+        if nodo.id == goal_node.id:
+            explored[nodo.id] = nodo
+            # print(cost, nodo)
+            # print(path)
+
+            # print(latest_cost)
+            return str(latest_cost)+"\n"+"; ".join([str(x) for x in (path)])
+        # print("expanded ___!", nodo, cost)
+        for t, n in nodo.conections.items():  # expansion
+            conex = n
+            cost_n = t[2]
+            trans = (t[0], t[1])
+            n_path = list(path)
+            # print(n_path)
+            n_path.append(trans)
+
+            if conex.id not in explored:  # or conex not frontier:
+                # print(conex)
+                pq.add((conex, cost_n+latest_cost,
+                        n_path, gsp.heuristic[conex.id]))
+
+
+def greedy_evaluation(m1, g):
+    for i in range(len(g)):
+        if g[i] != ['X'] and m1[i] != g[i]:
+            return False
+    return True
+
+
+def greedy_ucs(start_node, goal_node):
+    explored = {}
+    pq = PriorityQueue()
+    # nodo, currentsume, path, heuristica
+    pq.add((start_node, 0, [], 0))
+    while(True):
+        # print(str(pq))
+        if pq.is_empty() == True:
+            return "No solution found"
+        nodo, latest_cost, path = pq.pop()
+        #print("...", path)
+        #print(greedy_evaluation(nodo.structure, goal_node.structure))
+        if (greedy_evaluation(nodo.structure, goal_node.structure) == True):
+            explored[nodo.id] = nodo
+            # print(cost, nodo)
+            # print(path)
+
+            # print(latest_cost)
+            return str(latest_cost)+"\n"+"; ".join([str(x) for x in (path)])
+        # print("expanded ___!", nodo, cost)
+        for t, n in nodo.conections.items():  # expansion
+            conex = n
+            cost_n = t[2]
+            trans = (t[0], t[1])
+            n_path = list(path)
+            # print(n_path)
+            n_path.append(trans)
+
+            if conex.id not in explored:  # or conex not frontier:
+                # print(conex)
+                pq.add((conex, cost_n+latest_cost, n_path, 0))
+
+
+def greedy_a_star(start_node, goal_node, gsp):
+    explored = {}
+    pq = PriorityQueue()
+    # nodo, currentsume, path, heuristica
+    pq.add((start_node, 0, [], gsp.heuristic[start_node.id]))
+    while(True):
+        # print(str(pq))
+        if pq.is_empty() == True:
+            return "No solution found"
+        nodo, latest_cost, path = pq.pop()
+        #print("...", path)
+        #print((nodo.structure, goal_node.structure))
+
+        if greedy_evaluation(nodo.structure, goal_node.structure) == True:
+            explored[nodo.id] = nodo
+            # print(cost, nodo)
+            # print(path)
+
+            # print(latest_cost)
+            return str(latest_cost)+"\n"+"; ".join([str(x) for x in (path)])
+        # print("expanded ___!", nodo, cost)
+        for t, n in nodo.conections.items():  # expansion
+            conex = n
+            cost_n = t[2]
+            trans = (t[0], t[1])
+            n_path = list(path)
+            # print(n_path)
+            n_path.append(trans)
+
+            if conex.id not in explored:  # or conex not frontier:
+                # print(conex)
+                pq.add((conex, cost_n+latest_cost,
+                        n_path, gsp.heuristic[conex.id]))
+
+
+class PriorityQueue():
+
+    def is_empty(self):
+        if len(self.q) == 0:
+            return True
+        return False
+
+    def __init__(self):
+        self.q = deque([])
+
+    def __str__(self):
+        s = "\n-----------\n"
+        for i in self.q:
+            s = s+str(i[0])+", "+str(i[1])+";\n"
+        return s
+
+    def pop(self):
+        self.q = deque(self.q)
+        a = self.q.popleft()
+        return a[0], a[1], (a[2])
+
+    def add(self, paired):
+        self.q.append(paired)
+        self.q = sorted(self.q, key=lambda x: (x[1]+x[3]))
 
 
 def main():
-    '''
+    # PARSING INPUT
     max_height = int(input())
     start_structure = str(input())
     goal_structure = str(input())
     start_structure = start_structure.strip().split(";")
     goal_structure = goal_structure.strip().split(";")
-    start_structure = list(map(lambda x: x.strip().strip("(").strip(")"), start_structure))
-    goal_structure = list(map(lambda x: x.strip().strip("(").strip(")"), goal_structure))
-    start_structure = list(map(lambda x: x.split(", ") if x != "X" else [], start_structure))
-    goal_structure = list(map(lambda x: x.split(", ") if x != "X" else [], goal_structure))
-    print(goal_structure)
-    print(start_structure)
-    gsp=GraphSearchSpace()
-    initialState = Node(start_structure, max_height)
+    start_structure = list(
+        map(lambda x: x.strip().strip("(").strip(")"), start_structure))
+    goal_structure = list(
+        map(lambda x: x.strip().strip("(").strip(")"), goal_structure))
+    start_structure = list(map(lambda x: x.split(
+        ", ") if x != '' else [], start_structure))
+    goal_structure = list(map(lambda x: x.split(
+        ", ") if x != '' else [], goal_structure))
+
+    # DEBUG PAARSING
+    # print('G?->', goal_structure)
+    # print('S?->', start_structure)
+    # Initial Node Creation
+
+    # Dummy Node for Debug
+    # max_height = 2
+    # goal_structure = [['A', 'C'], [], []]
+    # start_structure = [['A'], ['B'], ['C']]
+
+    # Graph Search Space Creation
+    gsp = GraphSearchSpace(max_height)
+    initialState = Node(start_structure)
+    goalState = Node(goal_structure)
     initialState.generate_paths(gsp)
-    '''
 
-    max_height = 2
-    goal_structure = [['A', 'C'], [], []]
-    start_structure = [['A'], ['B'], ['C']]
-    gsp = GraphSearchSpace()
-    initialState = Node(start_structure, max_height)
-    # a=set()
-    # a.add(initialState)
-    # second = Node(start_structure, max_height)
-    # print(second in a)
+    greedy = False
+    for l in goal_structure:
+        for i in l:
+            if i == "X":
+                greedy = True
+                break
+    # print(greedy)
 
+    # Execute generation of path for Initial node
 
+    if greedy == True:
+        print(greedy_ucs(initialState, goalState))
+        # gsp.generate_heuristic(goalState.structure)
+        #print(greedy_a_star(initialState, goalState, gsp))
 
-    initialState.generate_paths(gsp)
-    #print("kmkm")
-    print(gsp.__str__())
-    for i in initialState.conections:
-        print(i)
+    else:
+        if goalState.id in gsp.nodes:
+            print(ucs(initialState, goalState))
+            # gsp.generate_heuristic(goalState.structure)
+            #print(a_star(initialState, goalState, gsp))
+        else:
+            print("No solution found")
+
+        #print(a_star(initialState, goalState))
+
+    # for k,v in initialState.conections.items():
+    #   print(k,v )
+
+    # print(initialState)
+
+    # print("kmkm")
+    # print(gsp.__str__())
+    # for i in initialState.conections:
+    # print(i)
 
 
 if __name__ == "__main__":
     main()
-
 
 #import fileinput
 
@@ -163,70 +349,3 @@ if __name__ == "__main__":
 #    lines.append(line.strip())
 # num=list(map(int,lines[0]))[0]
 # print(sum(num))
-
-'''
-
-def __init__(self, foo, bar):
-        self.foo = foo
-        self.bar = bar
-    def __repr__(self):
-        return "Item(%s, %s)" % (self.foo, self.bar)
-    def __eq__(self, other):
-        if isinstance(other, Item):
-            return ((self.foo == other.foo) and (self.bar == other.bar))
-        else:
-            return False
-    def __ne__(self, other):
-        return (not self.__eq__(other))
-    def __hash__(self):
-        return hash(self.__repr__())
-
-
-
-class SearchSpace():
-    def __init__(self):
-        self.nodes = set()
-        self.edges = set()
-
-
-class Node:
-
-    def __init__(self, structure, h):
-        self.num_stacks = len(structure)
-        self.max_height = h
-        self.structure = structure
-
-#   def looking_transition(self. origin_stack, destination_stack):
-
-
-
-def main():
-
-    a = int(input())
-    start = str(input())
-    goal = str(input())
-    start = start.strip().split(";")
-    goal = goal.strip().split(";")
-    start = list(map(lambda x: x.strip().strip("(").strip(")"), start))
-    goal = list(map(lambda x: x.strip().strip("(").strip(")"), goal))
-    start = list(map(lambda x: x.split(", ") if x != "X" else [], start))
-    goal = list(map(lambda x: x.split(", ") if x != "X" else [], goal))
-    print(start)
-    print(goal)
-    
-
-from collections import deque
-
-def generate_iterations(size_structure):
-    q=deque([i for i in range(0,size_structure)])
-    for i in range(0,size_structure):
-        e=q.popleft()
-        q.append(e)
-        for j in range(1+i,len(q)):
-            print(i,j)
-        
-        
-  
-    generate_iterations(4) 
-    
-'''
