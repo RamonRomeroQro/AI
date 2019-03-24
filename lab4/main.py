62fcc172dc35b8a26c0fb9a99cc680d6fd4f1399
RamonRomeroTec <rr100@live.com.mx>
2019-03-24 15:34:12 -0600

    1tc ok + added network

--- lab4/main.py
+++ lab4/main.py
@@ -1,4 +1,4 @@
-def total_probalility(calculated_values, variables, query):
+def total_probalility(probability_table, variables, query, bayes_network):
 	''' P(B) = P(B|A1)*P(A1) + P(B|A2)*P(A2) + ... + P(B|An)*P(An) '''
 
 	aux= query[1:]
@@ -6,23 +6,23 @@ def total_probalility(calculated_values, variables, query):
 	
 	for p in variables:
 		if p != aux:
-			positive, calculated_values = find_probability(query+'|'+'+'+p, calculated_values, variables)
-			calculated_values[query+'|'+'+'+p]= positive
+			positive, probability_table = find_probability(query+'|'+'+'+p, probability_table, variables, bayes_network)
+			probability_table[query+'|'+'+'+p]= positive
 
-			negative, calculated_values = find_probability(query+'|'+'-'+p, calculated_values, variables)
-			calculated_values[query+'|'+'-'+p]= negative
+			negative, probability_table = find_probability(query+'|'+'-'+p, probability_table, variables, bayes_network)
+			probability_table[query+'|'+'-'+p]= negative
 
-			positive_multiplicator, calculated_values = find_probability('+'+p, calculated_values, variables)
-			calculated_values['+'+p]=positive_multiplicator
+			positive_multiplicator, probability_table = find_probability('+'+p, probability_table, variables, bayes_network)
+			probability_table['+'+p]=positive_multiplicator
 
-			negative_multiplicator, calculated_values = find_probability('-'+p, calculated_values, variables)
-			calculated_values['-'+p]=negative_multiplicator
+			negative_multiplicator, probability_table = find_probability('-'+p, probability_table, variables, bayes_network)
+			probability_table['-'+p]=negative_multiplicator
 
 			total = total + (positive*positive_multiplicator)
 			total = total + (negative*negative_multiplicator)
 	
-	calculated_values[query]=total
-	return total, calculated_values
+	probability_table[query]=total
+	return total, probability_table
 
 
 			
@@ -30,26 +30,25 @@ def total_probalility(calculated_values, variables, query):
 
 
 
-def bayes_therorem(calculated_values, variables, query):
+def bayes_therorem(probability_table, variables, query, bayes_network):
 	''' P(A|B) = (P(B|A) * P(A)) / P(B) '''
 	subqueries = query.split("|")
-	# comma validation
 	first_numerator_query= "|".join(reversed(subqueries))
-	first_numerator, calculated_values= find_probability(first_numerator_query, calculated_values, variables)
-	calculated_values[first_numerator_query]=first_numerator
+	first_numerator, probability_table= find_probability(first_numerator_query, probability_table, variables, bayes_network)
+	probability_table[first_numerator_query]=first_numerator
 
 	second_numerator_query=subqueries[0]
-	second_numerator, calculated_values = find_probability(second_numerator_query, calculated_values, variables)
-	calculated_values[second_numerator_query]=second_numerator
+	second_numerator, probability_table = find_probability(second_numerator_query, probability_table, variables, bayes_network)
+	probability_table[second_numerator_query]=second_numerator
 
 	denominator_query = subqueries[-1]
-	denominator, calculated_values = find_probability(denominator_query, calculated_values, variables)
-	calculated_values[denominator_query]= denominator
+	denominator, probability_table = find_probability(denominator_query, probability_table, variables, bayes_network)
+	probability_table[denominator_query]= denominator
 
 	bayes = (first_numerator*second_numerator) / denominator
-	calculated_values[query]=bayes
+	probability_table[query]=bayes
 
-	return bayes, calculated_values
+	return bayes, probability_table
 
 def conditional_probability():
 	pass
@@ -57,80 +56,130 @@ def conditional_probability():
 def chain_rule():
 	pass
 
-def find_probability(query, calculated_values, variables):
+def find_probability(query, probability_table, variables, bayes_network):
 
-	if query in calculated_values:
-		return calculated_values[query] , calculated_values
+	if query in probability_table:
+		return probability_table[query] , probability_table
 	else:
 
 		negative_variable = '-'+query[1:]
 		positive_variable = '+'+query[1:]
 
 		# Find the negative compliment if the positive exists
-		if (query.startswith('+') and negative_variable in calculated_values):
-			calculated_complement = 1 - calculated_values[negative_variable]
-			calculated_values[positive_variable]=calculated_complement
-			return calculated_complement, calculated_values
+		if (query.startswith('+') and negative_variable in probability_table):
+			calculated_complement = 1 - probability_table[negative_variable]
+			probability_table[positive_variable]=calculated_complement
+			return calculated_complement, probability_table
 		
 		# Find the positive compliment if the negative exists
-		elif (query.startswith('-') and positive_variable in calculated_values):
-			calculated_complement = 1 - calculated_values[positive_variable]
-			calculated_values[negative_variable]=calculated_complement
-			return calculated_complement, calculated_values
+		elif (query.startswith('-') and positive_variable in probability_table):
+			calculated_complement = 1 - probability_table[positive_variable]
+			probability_table[negative_variable]=calculated_complement
+			return calculated_complement, probability_table
 
 		else:
 
-			
 			if query.find('|') == -1:
-				total, calculated_values = total_probalility(calculated_values, variables, query)
-				return total, calculated_values 
+				total, probability_table = total_probalility(probability_table, variables, query, bayes_network)
+				return total, probability_table 
 
 			else:
-				bayes, calculated_values = bayes_therorem(calculated_values, variables, query)
-				return bayes, calculated_values
+				bayes, probability_table = bayes_therorem(probability_table, variables, query, bayes_network)
+				return bayes, probability_table
 
 
+
+
+class Node():
+	def __init__(self, name):
+		''' Creation by name Node '''
+		self.name=name
+		self.parents={}
+
+class Network():
+	''' Nodes container '''
+	def __init__(self, set_string_node):
+		'''Given a string set, generate nodes'''
+		self.set_nodes={}
+		for i in set_string_node:
+			self.set_nodes[i]=Node(i)
+
+	def getNode(self, node_name):
+		''' Get node from network by Node'''
+		if node_name in self.set_nodes:
+			return self.set_nodes[node_name]
+		return None
 	
+	def parent_modification(self, name_node, name_node_parent):
+		''' Get a node and then add a parent'''
+		node_to_edit = self.getNode(name_node)
+		parent = self.getNode(name_node_parent)
+		if node_to_edit and parent:
+			node_to_edit.parents[name_node_parent]=parent
+
+	def __str__(self):
+		return "\n".join([ list(map(str, (k,v))) for k,v in self.set_nodes.items()])
+
+
 
 def main():
-	# Parsing input
-
+	# Getting node variables
 	variables = str(input())
 	variables = variables.split(",")
 	variables = set([x.strip() for x in variables])
-	amount_of_values = int(input())
 
-	calculated_values = {}
-	for i in range(amount_of_values):
+	# Build Node Network (1)
+	bayes_network = Network(variables)
+
+	# Get Probability table
+	amount_of_probabilities = int(input())
+	probability_table = {}
+
+	for i in range(amount_of_probabilities):
 		value_string = str(input())
 		value_string = [x.strip() for x in value_string.split("=")]
-		calculated_values[str(value_string[0])]=float(value_string[1])
-	
-	
+		key = str(value_string[0])
+		value = value_string[1]
+		probability_table[key]=float(value)
+
+		# checking substringing
+		
+		if key.find('|') != -1:
+			broken_key = key.split("|")
+			child_name = broken_key[0] 
+			parents_names = broken_key[1].split(",")
+			for p_name in parents_names:
+				if p_name.startswith('+') or p_name.startswith('-'):
+					p_name=p_name[1:]
+				if child_name.startswith('+') or child_name.startswith('-'):
+					child_name=child_name[1:]
+				bayes_network.parent_modification(child_name,p_name)
+
+
 	amount_of_queries = int(input())
 	queries = []
 	for i in range(amount_of_queries):
 		queries.append(str(input()))
 
-	print(calculated_values)
-	print(queries)
-	print(variables)
+	# print(probability_table)
+	# print(queries)
+	# print(variables)
 
-	#calculated_values = {'+Ill': 0.001, '+Test|+Ill': 0.9, '+Test|-Ill': 0.5}
-	#queries = ['+Ill', '-Ill', '+Ill|+Test', '+Test', '-Test', '+Test|+Ill', '+Test|-Ill']
-	#variables ={'Test', 'Ill'}
+	# probability_table = {'+Ill': 0.001, '+Test|+Ill': 0.9, '+Test|-Ill': 0.5}
+	# queries = ['+Ill', '-Ill', '+Ill|+Test', '+Test', '-Test', '+Test|+Ill', '+Test|-Ill']
+	# variables ={'Test', 'Ill'}
 
 	answers=[]
 
 	for query in queries:
-		answer, calculated_values = find_probability(query, calculated_values, variables)
+		answer, probability_table = find_probability(query, probability_table, variables, bayes_network)
 		answers.append(answer)
 
 
 	print("\n".join([str(x) for x in answers]))
 
 
-	# print (calculated_values)
+	# print (probability_table)
 
 
 
