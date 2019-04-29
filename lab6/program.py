e27eec2f947265b61427566c6908dd6efa056d5f
RamonRomeroTec <rr100@live.com.mx>
2019-04-29 01:49:17 -0500

    finished perceptron

--- lab6/program.py
+++ lab6/program.py
@@ -20,65 +20,69 @@
     Python 3.7.2 (PEP8)
 
 '''
+
+
 def dot_product(values, weights):
     ''' Dot Product operation'''
-    return sum(value * peso for value, peso in zip(values, weights))
+    return sum(v * w for v, w in zip(values, weights))
+
+class Perceptron():
+    def __init__(self, dimensionality):
+        ''' Perceptron initializer '''
+        self.threshold = 0.5
+        self.learning_rate = 0.01
+        self.weights = [0] * dimensionality
+        self.epoch_limit = 1000
+        self.limit = False
+
+    def training(self, train_expected):
+        ''' Train perceptron with array of values, expectation'''
+        epochs = 0
+        while True:
+            #print (epochs)
+            epochs = epochs+1
+            error_counter = 0
+            for inputs, expected_output in train_expected:
+
+                result = dot_product(inputs, self.weights) > self.threshold
+                error = expected_output - result
+                if error != 0:
+                    error_counter += 1
+                    for index, value in enumerate(inputs):
+                        self.weights[index] += self.learning_rate * \
+                            error * value
+            if error_counter == 0:
+                break
+            elif self.epoch_limit > 1000:
+                return None
+
+    def predict(self, inputs):
+        ''' Predict output for a given input '''
+        result = dot_product(inputs, self.weights) > self.threshold
+        return (int(result))
+
 
 def main():
-    ''' Perceptron '''
-    dimensionality=int(input())
-    size_training=int(input())
-    size_test=int(input())
-    training=[]
+    # Parsing input
+    dimensionality = int(input())
+    size_training = int(input())
+    size_test = int(input())
+    training = []
     for i in range(size_training):
-        a=[ float(x.strip()) for x in str(input()).split(',')]
-        t=a[:-1], a[-1]
+        a = [float(x.strip()) for x in str(input()).split(',')]
+        t = a[:-1], a[-1]
         training.append(t)
-    #print (training)
-    threshold = 0.5
-    learning_rate = 0.01
-    weights = [0] * dimensionality
-    epochs=0
-    limit=False
-
-
-    while True:
-        #print (epochs)
-        epochs=epochs+1
-        error_counter = 0
-        for inputs, expected_output in training:
-
-            result = dot_product(inputs, weights) > threshold
-            error = expected_output - result
-            if error != 0:
-                error_counter += 1
-                for index, value in enumerate(inputs):
-                    weights[index] += learning_rate * error * value
-        if error_counter == 0:
-            break
-        elif  epochs>1000:
-            limit=True
-            break
-
-    test=[]
-    for i in range(size_test):
-        b=[ float(x.strip()) for x in str(input()).split(',')]
-        test.append(b)
-
-    if limit==False:
-        #print(weights)
-
-        for t in test:
-            result = dot_product(t, weights) > threshold
-            print(int(result))
+    # print (training)
+    # Create Perceptron
+    p = Perceptron(dimensionality)
+    # Train Perceptron
+    if p.training(training) != None:
+        for i in range(size_test):
+            b = [float(x.strip()) for x in str(input()).split(',')]
+            # Precict Output
+            print(p.predict(b))
     else:
         print("no solution found")
-                
-        
-
-    
-    
-
 
 if __name__ == "__main__":
     main()
